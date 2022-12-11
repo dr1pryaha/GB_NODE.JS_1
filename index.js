6a21fc8bbb0631d32ae45a852175ccbb56e9c89a
Anton Pryakhin <dr.pryah@gmail.com>
2022-12-11 18:02:51 +0300

    lesson_1: add function simpleNumbers

--- index.js
+++ index.js
@@ -1,2 +1,52 @@
 import colors from "colors";
 console.log(colors.red("Hello World!"));
+
+const arg1 = Number(process.argv[2]);
+const arg2 = Number(process.argv[3]);
+
+let numbers = [];
+
+function simpleNumbers(arg1, arg2) {
+  if (arg2 > 1) {
+    nextVar: for (var i = arg1; i <= arg2; i++) {
+      for (var j = 2; j < i; j++) {
+        if (i % j == 0) {
+          continue nextVar;
+        }
+      }
+      numbers = [...numbers, i];
+    }
+  }
+
+  let nums = numbers.map((n, i) => {
+    if (i % 3 === 0) {
+      console.log(colors.red(n));
+    } else if (i % 3 === 1) {
+      console.log(colors.yellow(n));
+    } else {
+      console.log(colors.green(n));
+    }
+
+    return n;
+  });
+
+  if (
+    typeof arg1 !== "number" ||
+    isNaN(arg1) ||
+    typeof arg2 !== "number" ||
+    isNaN(arg2)
+  ) {
+    console.error(
+      colors.red(
+        "Диапазон должен содержать только цифры! Укажите другой диапазон"
+      )
+    );
+  } else if (numbers.length === 0) {
+    console.error(
+      colors.red("Простых чисел не найдено! Укажите другой диапазон")
+    );
+  }
+  console.log(nums);
+}
+
+simpleNumbers(arg1, arg2);
